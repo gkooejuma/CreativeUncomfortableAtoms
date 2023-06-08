51c5386e2c4c5954c69e0dec8e77379a1b0fa2f8
gkooejuma <132706088+gkooejuma@users.noreply.github.com>
2023-06-08 01:58:25 +0000

    dd

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello Express app!')
 });
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('server started');
 });
-//
 
-process.exit(); 
+
+//process.exit(); 
